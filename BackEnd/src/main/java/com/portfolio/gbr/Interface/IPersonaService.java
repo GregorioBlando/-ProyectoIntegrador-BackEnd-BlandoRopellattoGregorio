8b45134aafbfa0b8d73a37412d2ac92ef9b4d4e7
Gregorio Blando Ropellatto <gregorio.blando@mi.unc.edu.ar>
2023-01-08 22:57:07 -0300

    2do Commit
    Backend inicial

---
+++ BackEnd/src/main/java/com/portfolio/gbr/Interface/IPersonaService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.gbr.Interface;
+
+import com.portfolio.gbr.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    /* Traer una lista de personas */
+    public List<Persona> getPersona();
+    
+    /* Guardar un objeto de tipo persona */
+    public void savePersona(Persona persona);
+    
+    /* Eliminar un objeto por Id*/
+    public void deletePersona(Long id);
+    
+    /* Buscar una persona por Id */
+    public Persona findPersona(Long id);
+}
