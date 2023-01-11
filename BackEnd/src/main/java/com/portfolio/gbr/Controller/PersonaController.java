86b1e6515a520a0cb00e0c4c78ea9a0f72b27394
Gregorio Blando Ropellatto <gregorio.blando@mi.unc.edu.ar>
2023-01-11 16:15:39 -0300

    3er Commit

--- BackEnd/src/main/java/com/portfolio/gbr/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/gbr/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.gbr.Entity.Persona;
 import com.portfolio.gbr.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -51,6 +53,11 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
     
 }
